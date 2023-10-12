7d97a8f90bd36cbd14e71f8daa9f8b797602fca5
natureslayr <34704015+natureslayr@users.noreply.github.com>
2023-10-11 22:26:12 -0400

    Fix stat calculations
    Stat calculations calculate global and average daily mean, max, and min.
    At this stage, the code is functional and exports data properly.

--- converter.py
+++ converter.py
@@ -12,9 +12,7 @@ pressure = pressureData.iloc[:,1]
 
 def getDate(inputTime):
     global finalDateList
-    global monthDay
     finalDateList = []
-    monthDay = []
     for i in inputTime:
         dateFormat = "%m/%d/%Y %H:%M:%S %p"
         n = datetime.strptime(i,dateFormat).date()
@@ -32,6 +30,8 @@ def removeZeros(inputData,inputTime):
     global finalTime
     global finalPressure
     global finalDate
+    global monthDay
+    monthDay = []
     finalTime = []
     finalPressure = []
     finalDate = []
@@ -54,61 +54,117 @@ def cleanData(inputData,inputTime,inputDate):
 def getDailyAverage(uniqueData, inputTime, finalPressure):
     tempSum = []
     global dailyAverage
-    global dailyMax
-    global dailyMin
     dailyAverage = []
+    for i in uniqueData:
+        for idn, n in enumerate(inputTime):
+            if idn == (len(inputTime) - 1):
+                tempSum.append(finalPressure[inputTime.index(n)])
+                dailyAverage.append(np.average(tempSum))
+                tempSum.clear()
+            elif i == n:
+                tempSum.append(finalPressure[inputTime.index(n)])
+
+def getDailyMax(uniqueData, inputTime, finalPressure):
+    tempMax = 0
+    global dailyMax
     dailyMax = []
+    for i in uniqueData:
+        for idn, n in enumerate(inputTime):
+            if i == n:
+                if finalPressure[idn] > tempMax:
+                    tempMax = (finalPressure[idn])
+                    if idn == (len(inputTime) - 1):
+                        dailyMax.append(tempMax)
+                        tempMax = 0
+            else:
+                if idn == (len(inputTime) - 1):
+                    dailyMax.append(tempMax)
+                    tempMax = 0
+
+def getDailyMin(uniqueData, inputTime, finalPressure):
+    tempMin = 1000
+    global dailyMin
     dailyMin = []
     for i in uniqueData:
         for idn, n in enumerate(inputTime):
             if i == n:
+                if finalPressure[idn] < tempMin:
+                    tempMin = (finalPressure[idn])
+                    if idn == (len(inputTime) - 1):
+                        dailyMin.append(tempMin)
+                        tempMin = 1000
+            else:
                 if idn == (len(inputTime) - 1):
-                    tempSum.append(finalPressure[inputTime.index(n)])
-                    dailyAverage.append(np.average(tempSum))
-                    dailyMax.append(np.max(tempSum))
-                    dailyMin.append(np.min(tempSum))
-                    tempSum.clear()
-                else:
-                    tempSum.append(finalPressure[inputTime.index(n)])
+                    dailyMin.append(tempMin)
+                    tempMin = 1000
 
 def getAverage(input):
     global averageDailyMean
-    averageDailyMean = np.average(input)
+    averageDailyMean = round(np.average(input),1)
 
 def getMax(input):
     global averageDailyMax
-    averageDailyMax = np.average(input)
+    averageDailyMax = round(np.average(input),1)
 
 def getMin(input):
     global averageDailyMin
-    averageDailyMin = np.average(input) 
+    averageDailyMin = round(np.average(input),1)
+
+def getStats(input):
+    global globalMin
+    global globalMax
+    global globalMean
+    globalMin = round(np.min(input),1)
+    globalMax = round(np.max(input),1)
+    globalMean = round(np.average(input),1)
 
 getDate(time) 
 removeZeros(pressure, time)
 getUniqueDay(monthDay)
 getDailyAverage(uniqueDay, monthDay, finalPressure)
+getDailyMax(uniqueDay, monthDay, finalPressure)
+getDailyMin(uniqueDay, monthDay, finalPressure)
 getAverage(dailyAverage)
 getMax(dailyMax)
 getMin(dailyMin)
+getStats(finalPressure)
 
-print(dailyAverage)
-print(averageDailyMean)
-print(averageDailyMax)
-print(averageDailyMin)
+print("Daily Min: " + str(averageDailyMin))
+print("Daily Max: " + str(averageDailyMax))
+print("Daily Average "  + str(averageDailyMean))
+print(" ")
+print("Global Min: " + str(globalMin))
+print("Global Max: " + str(globalMax))
+print("Global Average: " + str(globalMean))
 
-#getAverage(dailyAverage)
-#print("Min: " + str(min))
-#print("Max: " + str(max))
-#print("Average "  + str(dailyMean))
-data = {"Date of Reading": finalDate, "Time (Days)": finalTime, "Pressure": finalPressure,}
+data = {
+    "Date of Reading": pd.Series(finalDate), 
+    "Time (Days)": pd.Series(finalTime), 
+    "Pressure": pd.Series(finalPressure), 
+    "Daily Average": pd.Series(averageDailyMean), 
+    "Daily Min": pd.Series(averageDailyMin), 
+    "Daily Max": pd.Series(averageDailyMax),
+    "Global Average": pd.Series(globalMean),
+    "Global Min": pd.Series(globalMin),
+    "Global Max": pd.Series(globalMax)
+    }
 
 df = pd.DataFrame(data)
 df.to_csv(r"C:\Users\tgalyon\Documents\python\data_directory\out.csv")
 
-plt.plot(finalTime,finalPressure)
-plt.title("Pressure vs. Time")
-plt.xlabel("Time (Days)")
-plt.ylabel("Pressure (psi)")
+plt.plot(finalTime,finalPressure, color = "blue")
+plt.title("Pressure vs. Time", size = 18)
+plt.xlabel("Time (Days)", size = 12)
+plt.ylabel("Pressure (psi)", size = 12)
 plt.xticks(np.arange(np.min(finalTime), np.max(finalTime), 1))
-plt.grid()
+plt.xlim(min(finalTime),max(finalTime))
+plt.rc('lines', linewidth = 4)
+plt.grid(linestyle = "--")
+plt.rcParams['figure.dpi']=360
+plt.text(x=1, y=min(finalPressure), s="Average Daily Max: " + str(averageDailyMax), \
+    color = "black", fontsize = 10)
+plt.text(x=1, y=min(finalPressure) + 5, s="Average Daily Min: " + str(averageDailyMin), \
+    color = "black", fontsize = 10)
+plt.text(x=1, y=min(finalPressure) + 10, s="Average Daily Mean: " + str(averageDailyMean), \
+    color = "black", fontsize = 10)
 plt.show()
